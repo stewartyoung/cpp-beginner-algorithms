c8bd0d0dda2b152b21c32100a22afae367be25ff
Stewart Young <stewartyoung97@gmail.com>
2019-10-11 21:31:58 +0100

    First attempt at making tree, but not working

--- TreesAndStars.cpp
+++ TreesAndStars.cpp
@@ -5,5 +5,20 @@ int main(){
         if (i == 1){
             cout << "     *     \n";
         };
+        if ((i>1) && (i<6)){
+            // print 6 - i spaces at the beginning of each new line
+            string spaceString = " ";
+            std:string leftSpaces(i-1, spaceString);
+            cout << "/";
+            // print i - 2 spaces after left side of tree
+            std::string MiddleLeftSpaces(i - 2, " ");
+            cout << "|";
+            // print i -2 spaces after middle of tree
+            std::string MiddleRightSpaces(i - 2, " ");
+            // print the right hand side of the tree
+            cout << "\\";
+            // end the line
+            cout << "\n";
+        };
     };
 }
