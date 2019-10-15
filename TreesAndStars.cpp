867859f3cdd95e42e6edc9292289de887b54b090
Stewart Young <stewartyoung97@gmail.com>
2019-10-15 08:40:45 +0100

    add working trees and stars (no for loop)

--- TreesAndStars.cpp
+++ TreesAndStars.cpp
@@ -1,24 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    for(int i=1; i<= 6; i++){
-        if (i == 1){
-            cout << "     *     \n";
-        };
-        if ((i>1) && (i<6)){
-            // print 6 - i spaces at the beginning of each new line
-            string spaceString = " ";
-            std:string leftSpaces(i-1, spaceString);
-            cout << "/";
-            // print i - 2 spaces after left side of tree
-            std::string MiddleLeftSpaces(i - 2, " ");
-            cout << "|";
-            // print i -2 spaces after middle of tree
-            std::string MiddleRightSpaces(i - 2, " ");
-            // print the right hand side of the tree
-            cout << "\\";
-            // end the line
-            cout << "\n";
-        };
-    };
-}
+            cout << "      *     \n";
+            cout << "     /|\\     \n";
+            cout << "    / | \\    \n";
+            cout << "   /  |  \\      \n";
+            cout << "  /   |   \\  \n";
+            cout << " /____|____\\ \n";
+};
