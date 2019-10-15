8bd06eb0908772218ef5d89ae6776975b6f10bcc
Stewart Young <stewartyoung97@gmail.com>
2019-10-15 08:54:24 +0100

    Add increasing stars for loop

--- StarsReadingInput.cpp
+++ StarsReadingInput.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main(){
     for (int i = 1; i <= 5; i++){
-        cout << "*";
+        
+        for (int j = 1; j <= i; j++){
+            cout << "*";
+        };
         cout << endl;
     };
 };
